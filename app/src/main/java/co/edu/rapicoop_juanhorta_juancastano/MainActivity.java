6614375d517e8f7ec52696e3487a449641b2d35b
Juan Sebastián Castaño Ramírez <jscr00@hotmail.com>
2022-09-01 17:59:58 -0500

    Initial commit

---
+++ app/src/main/java/co/edu/rapicoop_juanhorta_juancastano/MainActivity.java
@@ -0,0 +1,14 @@
+package co.edu.rapicoop_juanhorta_juancastano;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
